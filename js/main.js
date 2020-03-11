f0ffa07708dbb78a043d0026ecd94e03acde6261
unknown <world4den@mail.ru>
2020-03-11 16:04:22 +0600

    fixes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+'use strict';
 var mySwiper = new Swiper('#header-slider', {
     spaceBetween: 30,
     loop: true,
@@ -43,10 +44,62 @@ var postsSwiper = new Swiper('#posts-slider', {
   }
 });
 
+const typeSelection = document.querySelector('.type-selection'),
+      projectCards = document.querySelectorAll('.project-card'),
+      menuButton = document.querySelector('.menu-button'),
+      menu = document.querySelector('.navbar'),
+      projectsBtn = document.querySelector('.projects-btn');
 
-var menuButton = document.querySelector('.menu-button');
-var menu = document.querySelector('.navbar');
-menuButton.addEventListener('click', function(){
+
+
+const cardsLoadLimiter = type => {
+  projectCards.forEach(item => {
+    item.style.display = 'none';
+  });
+  if(type !== 'all'){
+    let typeQuery = document.querySelectorAll(`div[data-card-type=${type}]`);
+    if(typeQuery.length > 6){
+      for(let i = 0; i < 6; i++){
+        typeQuery[i].style.display = 'block';
+      }
+    } else{
+        typeQuery.forEach(element => {
+          element.style.display = 'block';
+      });
+    }
+  } else{
+    for(let i = 0; i < 6; i++){
+      projectCards[i].style.display = 'block';
+    }
+  }
+};
+
+
+
+menuButton.addEventListener('click', () => {
   menuButton.classList.toggle('menu-button-active');
   menu.classList.toggle('header-active');
-})
+});
+
+projectsBtn.addEventListener('click', () => {
+  let type = document.querySelector('input[type="radio"]:checked').value;
+  let typeQuery = document.querySelectorAll(`div[data-card-type=${type}]`);
+/*   var loadedCards = 6;
+  if(typeQuery.length > 6){
+    for(let i = loadedCards; i < loadedCards + 6; i++){
+      typeQuery[i].style.display = 'block';
+    }
+    loadedCards += 6;
+  } else{
+      console.log('nothing more');
+  } */
+
+});
+
+
+
+
+typeSelection.addEventListener('change', () => cardsLoadLimiter(event.target.value));
+
+
+window.onload = cardsLoadLimiter('all');  
